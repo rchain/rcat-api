ea94695123ffe6222d1435f0a297de4ab20a2238
Dragan Mijatovic <gandra404@yahoo.com>
2019-04-18 01:09:20 +0200

    appVersionTag

--- models/song.js
+++ models/song.js
@@ -286,7 +286,7 @@ songSchema.methods.transformCollaborators = function () {
 
 songSchema.methods.transformForAcquisition = function (user) {
 
-    const appVersionTag = '0.1.0';
+    const appVersionTag = '0.2.1';
 
     const timestasmp = + new Date();
 
