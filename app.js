55bb45311c5dc66d18c8b0f0080e30e2f408a6f4
Dragan Mijatovic <gandra404@yahoo.com>
2019-04-18 14:54:50 +0200

    payload size trying to fix

--- app.js
+++ app.js
@@ -15,8 +15,8 @@ const corsOption = {
     exposedHeaders: ['x-auth-token']
 };
 app.use(cors(corsOption));
-app.use(express.json({limit: '50mb'}));
-app.use(express.urlencoded({ extended: false, limit: '50mb' }));
+app.use(express.json({limit: '250mb'}));
+app.use(express.urlencoded({ extended: false, limit: '250mb' }));
 
 ///////////////////////////
 // Logging
