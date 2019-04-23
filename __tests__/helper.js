01b25afa8d31397a54497be45448dd47f1bed7b4
Dragan Mijatovic <gandra404@yahoo.com>
2019-04-23 13:54:30 +0200

    Fix tests

--- __tests__/helper.js
+++ __tests__/helper.js
@@ -2,8 +2,10 @@ const jwt = require("jsonwebtoken");
 
 const createJwtToken = () => {
     const jwtPayload = {
-        id:  123,
-        email: 'pera@lozac.com'
+        id:  '5cbee42c825f012dd7ac60d7',
+        email: 'test@test.com',
+        name: 'Jest tester',
+        auth_provider: 'pera@lozac.com'
     };
     const jwtOptions = require('../config/jwt-options');
     return jwt.sign(jwtPayload, process.env.JWT_SECRET, jwtOptions);
