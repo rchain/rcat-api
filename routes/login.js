b5a953a80f0681db463bcf8766cc4e9bc4a2436e
Dragan Mijatovic <gandra404@yahoo.com>
2019-04-24 16:00:32 +0200

    temp fb force err

--- routes/login.js
+++ routes/login.js
@@ -47,6 +47,9 @@ const requestFacebookSchema = {
 
 router.post('/facebook', validate(requestFacebookSchema), async (req, res, next) => {
     try {
+        if(req.body.force_error) {
+            return res.status(400).send({"statusCode":400,"error":"Bad Request","message":"Token mismatch"});
+        }
         const data = await facebookController.loginFacebook(req, res);
         if(data.statusCode) {
             console.log('facebookController.loginFacebook error response >>>', data);
